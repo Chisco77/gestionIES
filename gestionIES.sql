a045e583389f763893218c27c0244410044e4a0a
chisco <fmendezpalma@gmail.com>
2025-10-01 12:28:38 +0200

    Esquema base de datos

--- gestionIES.sql
+++ gestionIES.sql
@@ -1,71 +1,433 @@
-version: '3.8'
+--
+-- PostgreSQL database dump
+--
 
-services:
-  # 1. Servicio de Base de Datos PostgreSQL
-  db:
-    image: postgres:15-alpine
-    container_name: postgres_gestionIES
-    restart: always
-    environment:
-      # Lee variables de entorno de .env
-      POSTGRES_USER: ${DB_USER}
-      POSTGRES_PASSWORD: ${DB_PASSWORD}
-      POSTGRES_DB: ${DB_NAME}
-    # Montar un volumen para persistir los datos de la base de datos
-    volumes:
-      - postgres_data:/var/lib/postgresql/data
-      - ./db-init:/docker-entrypoint-initdb.d:ro
-    healthcheck:
-      test: ["CMD-SHELL", "pg_isready -U postgres -d gestionIES"]
-      interval: 5s
-      timeout: 5s
-      retries: 5
-    # No es necesario exponer el puerto al host si solo el backend accede a él
-    # ports:
-    #   - "5432:5432"
+\restrict BslpZ9zUeWNMXFUtRoSn8vnFBCOPk8K31WyK9nhVhm2oiPQbdrdrmSs2p1j718h
 
-  # 2. Servicio de Backend (Node.js)
-  backend_app:
-    build:
-      context: .
-      dockerfile: backend/Dockerfile
-    image: gestionies_backend_app:latest
-    container_name: node_gestionIES
-    restart: always
-    # Cargar variables de entorno del archivo .env del backend
-    env_file:
-      - backend/.env
-    # Mapeo de puertos, solo para pruebas o si necesitas acceder directamente. 
-    # Nginx hará el proxy, por lo que no es estrictamente necesario mapear 5000
-    # ports:
-    #   - "5000:5000"
-    depends_on:
-      - db # Asegura que la DB se inicie antes que el backend
-    environment:
-      # Sobrescribir el host de la DB para usar el nombre del servicio de docker-compose
-      DB_HOST: db
-      # NOTA: La configuración LDAP (`LDAP_URL`) no necesita cambiar si el servidor
-      # LDAP (172.16.218.2) está en la red física y Docker usa 'host' o una red 'bridge'
-      # que permite el acceso a la subred 172.16.X.X.
-      # Si da problemas, puedes añadir network_mode: "host" (solo en Linux).
+-- Dumped from database version 14.19 (Ubuntu 14.19-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 16.10 (Ubuntu 16.10-1.pgdg22.04+1)
 
-  # 3. Servicio de Frontend/Nginx
-  frontend_nginx:
-    build:
-      context: .
-      dockerfile: Dockerfile.frontend
-    container_name: nginx_gestionIES
-    restart: always
-    # Exponer al puerto 443 y 80 del host
-    ports:
-      - "80:80"
-      - "443:443"
-    depends_on:
-      - backend_app
-    # Montar certificados SSL autofirmados desde el host
-    volumes:
-      - /etc/nginx/ssl/nginx.crt:/etc/nginx/ssl/nginx.crt:ro
-      - /etc/nginx/ssl/nginx.key:/etc/nginx/ssl/nginx.key:ro
-      
-volumes:
-  postgres_data:
+-- Started on 2025-10-01 12:24:39 CEST
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 4 (class 2615 OID 2200)
+-- Name: public; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+-- *not* creating schema, since initdb creates it
+
+
+ALTER SCHEMA public OWNER TO postgres;
+
+--
+-- TOC entry 209 (class 1259 OID 16552)
+-- Name: api_cursos_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.api_cursos_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.api_cursos_id_seq OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 210 (class 1259 OID 16553)
+-- Name: cursos; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.cursos (
+    id bigint NOT NULL,
+    curso character varying NOT NULL
+);
+
+
+ALTER TABLE public.cursos OWNER TO postgres;
+
+--
+-- TOC entry 211 (class 1259 OID 16558)
+-- Name: cursos_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.cursos ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.cursos_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 212 (class 1259 OID 16559)
+-- Name: estancias; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.estancias (
+    id integer NOT NULL,
+    planta text NOT NULL,
+    codigo text NOT NULL,
+    descripcion text NOT NULL,
+    totalllaves integer DEFAULT 1 NOT NULL,
+    coordenadas_json jsonb NOT NULL
+);
+
+
+ALTER TABLE public.estancias OWNER TO postgres;
+
+--
+-- TOC entry 213 (class 1259 OID 16565)
+-- Name: estancias_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.estancias_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.estancias_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 3458 (class 0 OID 0)
+-- Dependencies: 213
+-- Name: estancias_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.estancias_id_seq OWNED BY public.estancias.id;
+
+
+--
+-- TOC entry 214 (class 1259 OID 16566)
+-- Name: libros; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.libros (
+    id bigint NOT NULL,
+    idcurso bigint NOT NULL,
+    libro character varying NOT NULL
+);
+
+
+ALTER TABLE public.libros OWNER TO postgres;
+
+--
+-- TOC entry 215 (class 1259 OID 16571)
+-- Name: libros_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.libros ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.libros_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 216 (class 1259 OID 16572)
+-- Name: perfiles_usuario; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.perfiles_usuario (
+    id integer NOT NULL,
+    uid character varying(255) NOT NULL,
+    perfil character varying(50) NOT NULL
+);
+
+
+ALTER TABLE public.perfiles_usuario OWNER TO postgres;
+
+--
+-- TOC entry 217 (class 1259 OID 16575)
+-- Name: perfiles_usuario_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.perfiles_usuario_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.perfiles_usuario_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 3459 (class 0 OID 0)
+-- Dependencies: 217
+-- Name: perfiles_usuario_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.perfiles_usuario_id_seq OWNED BY public.perfiles_usuario.id;
+
+
+--
+-- TOC entry 218 (class 1259 OID 16576)
+-- Name: prestamos; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.prestamos (
+    id bigint NOT NULL,
+    uid character varying NOT NULL,
+    esalumno boolean DEFAULT true,
+    doc_compromiso integer DEFAULT 0,
+    fechaentregadoc date,
+    fecharecepciondoc date,
+    iniciocurso boolean DEFAULT false NOT NULL
+);
+
+
+ALTER TABLE public.prestamos OWNER TO postgres;
+
+--
+-- TOC entry 219 (class 1259 OID 16584)
+-- Name: prestamos_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.prestamos ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.prestamos_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 220 (class 1259 OID 16585)
+-- Name: prestamos_items_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.prestamos_items_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.prestamos_items_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 221 (class 1259 OID 16586)
+-- Name: prestamos_items; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.prestamos_items (
+    id bigint DEFAULT nextval('public.prestamos_items_id_seq'::regclass) NOT NULL,
+    idprestamo bigint NOT NULL,
+    idlibro bigint NOT NULL,
+    fechaentrega date,
+    fechadevolucion date,
+    devuelto boolean DEFAULT false NOT NULL,
+    entregado boolean DEFAULT false NOT NULL
+);
+
+
+ALTER TABLE public.prestamos_items OWNER TO postgres;
+
+--
+-- TOC entry 222 (class 1259 OID 16592)
+-- Name: prestamos_llaves; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.prestamos_llaves (
+    id integer NOT NULL,
+    idestancia integer NOT NULL,
+    unidades integer DEFAULT 1 NOT NULL,
+    fechaentrega timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    fechadevolucion timestamp with time zone,
+    uid character varying NOT NULL
+);
+
+
+ALTER TABLE public.prestamos_llaves OWNER TO postgres;
+
+--
+-- TOC entry 223 (class 1259 OID 16599)
+-- Name: prestamos_llaves_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.prestamos_llaves ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.prestamos_llaves_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 224 (class 1259 OID 16600)
+-- Name: session; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.session (
+    sid character varying NOT NULL,
+    sess json NOT NULL,
+    expire timestamp(6) without time zone NOT NULL
+);
+
+
+ALTER TABLE public.session OWNER TO postgres;
+
+--
+-- TOC entry 3282 (class 2604 OID 16605)
+-- Name: estancias id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.estancias ALTER COLUMN id SET DEFAULT nextval('public.estancias_id_seq'::regclass);
+
+
+--
+-- TOC entry 3284 (class 2604 OID 16606)
+-- Name: perfiles_usuario id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.perfiles_usuario ALTER COLUMN id SET DEFAULT nextval('public.perfiles_usuario_id_seq'::regclass);
+
+
+--
+-- TOC entry 3294 (class 2606 OID 16608)
+-- Name: cursos cursos_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.cursos
+    ADD CONSTRAINT cursos_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3296 (class 2606 OID 16610)
+-- Name: estancias estancias_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.estancias
+    ADD CONSTRAINT estancias_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3298 (class 2606 OID 16612)
+-- Name: libros libros_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.libros
+    ADD CONSTRAINT libros_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3300 (class 2606 OID 16614)
+-- Name: perfiles_usuario perfiles_usuario_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.perfiles_usuario
+    ADD CONSTRAINT perfiles_usuario_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3302 (class 2606 OID 16616)
+-- Name: perfiles_usuario perfiles_usuario_uid_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.perfiles_usuario
+    ADD CONSTRAINT perfiles_usuario_uid_key UNIQUE (uid);
+
+
+--
+-- TOC entry 3306 (class 2606 OID 16618)
+-- Name: prestamos_items prestamos_items_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.prestamos_items
+    ADD CONSTRAINT prestamos_items_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3308 (class 2606 OID 16620)
+-- Name: prestamos_llaves prestamos_llaves_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.prestamos_llaves
+    ADD CONSTRAINT prestamos_llaves_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3304 (class 2606 OID 16622)
+-- Name: prestamos prestamos_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.prestamos
+    ADD CONSTRAINT prestamos_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3311 (class 2606 OID 16624)
+-- Name: session session_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.session
+    ADD CONSTRAINT session_pkey PRIMARY KEY (sid);
+
+
+--
+-- TOC entry 3309 (class 1259 OID 16625)
+-- Name: IDX_session_expire; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX "IDX_session_expire" ON public.session USING btree (expire);
+
+
+--
+-- TOC entry 3312 (class 2606 OID 16626)
+-- Name: prestamos_llaves prestamos_llaves_idestancia_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.prestamos_llaves
+    ADD CONSTRAINT prestamos_llaves_idestancia_fkey FOREIGN KEY (idestancia) REFERENCES public.estancias(id) ON DELETE CASCADE;
+
+
+--
+-- TOC entry 3457 (class 0 OID 0)
+-- Dependencies: 4
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE USAGE ON SCHEMA public FROM PUBLIC;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+-- Completed on 2025-10-01 12:24:39 CEST
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict BslpZ9zUeWNMXFUtRoSn8vnFBCOPk8K31WyK9nhVhm2oiPQbdrdrmSs2p1j718h
+
